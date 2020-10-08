88263f8d984c94c27ccbe3c3f239b485f1e42890
manas-debug <manassanas2@gmail.com>
2020-10-08 12:47:51 +0530

    1.0.1

--- App2/App.js
+++ App2/App.js
@@ -6,8 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,23 +22,27 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+import Home from './src/component/Home/Home'
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const MaterialBottomTabs = createMaterialBottomTabNavigator();
+const MaterialTopTabs = createMaterialTopTabNavigator();
+
 export default class App extends Component{
   render(){
 
     createHomeStack = () =>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component = { feed } />
-      <Stack.Screen name="Top Tabs" children = { CreateTopTab } />
-      <Stack.Screen name="Bottom Tabs" children = { CreateBottomTab } />
+      <Stack.Screen name="Home" component = { Home } />
     </Stack.Navigator>
 
     return(
       <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" children={createHomeStack} />
-        <Drawer.Screen name="Bottom Tabs" component={CreateBottomTab}/>
-        <Drawer.Screen name="Favorites" component={Screen1}/>
-        <Drawer.Screen name="Settings" component={Screen1}/>
+        <Drawer.Screen name="Login" children={createHomeStack} />
+        <Drawer.Screen name="About" children={createHomeStack}/>
       </Drawer.Navigator>
     </NavigationContainer>
     )
